01bb201196e9c018d8b2be7d8d6b02cc250dda72
Kalash Desai <kalashdesai.kd@gmail.com>
2026-06-27 13:32:02 +0530

    fix: remove modules

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const authRouter = require("./router/auth");
 const habitLogRouter = require("./router/habitLog");
 const { limiter, authLimiter } = require('./middleware/rateLimiter');
 
-connectToMongoDB("mongodb://localhost:27017/habit-tracker").then(() => console.log("DB connected")).catch((err) => console.error(`failed due to ${err}`));
+connectToMongoDB(process.env.MONGODB_URI).then(() => console.log("DB connected")).catch((err) => console.error(`failed due to ${err}`));
 app.use("/habits", limiter, router);
 app.use("/auth", limiter, authRouter);
 app.use("/habits", limiter, habitLogRouter);
